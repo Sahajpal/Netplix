76b8085e719f7d10711a354208ba26339426caef
Sahajpal <sahajpal80@gmail.com>
2021-12-17 19:15:47 +0530

    implemented NavBar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAhivccCV3aIK0SVTNjNx69yK9kJEI7AOY",
+    authDomain: "netflix-clone-76ebd.firebaseapp.com",
+    projectId: "netflix-clone-76ebd",
+    storageBucket: "netflix-clone-76ebd.appspot.com",
+    messagingSenderId: "518059927679",
+    appId: "1:518059927679:web:88786c00a71baefb33774a"
+  };
