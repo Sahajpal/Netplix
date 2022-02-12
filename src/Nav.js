d1f91e9a885c77a6f35540f6ef715f14fa08237a
Sahajpal <sahajpal80@gmail.com>
2022-02-12 11:44:34 +0530

    implemented Banner

--- src/Nav.js
+++ src/Nav.js
@@ -3,23 +3,23 @@ import "./Nav.css";
 import { useEffect, useState } from "react";
 
 function Nav() {
-const [show, handleShow] = useState(false);
+  const [show, handleShow] = useState(false);
 
-const transitionNavBar = () => {
-  if(window.scrollY > 100){
-    handleShow(true);
-  } else {
-    handleShow(false);
-  }
-}
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
 
-useEffect(() => {
-  window.addEventListener("scroll", transitionNavBar);
-  return () => window.removeEventListener("scroll", transitionNavBar);
-}, []);
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
 
   return (
-    <div className={`nav ${show && 'nav_black'}`}>
+    <div className={`nav ${show && "nav_black"}`}>
       <div className="nav_contents">
         <img
           className="nav_logo"
