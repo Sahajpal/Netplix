2863a9bab8925620126f88081be79f4cf1abc796
Sahajpal <sahajpal80@gmail.com>
2022-04-07 16:50:23 +0530

    implemented SignUpScreen

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -3,7 +3,18 @@ import './SignUpScreen.css'
 
 function SignUpScreen() {
   return (
-    <div className='signUpScreen'></div>
+    <div className='signUpScreen'>
+        <form>
+            <h1>Sign In</h1>
+            <input placeholder='Email' type='email'></input>
+            <input placeholder='Password' type='password'></input>
+            <button type='submit'>Sign In</button>
+            <h4>
+                <span className='signUpScreen_grey'>New to Netflix? </span>
+                <span className='signUpScreen_link'>Sign Up now.</span>
+            </h4>
+        </form>
+    </div>
   )
 }
 
