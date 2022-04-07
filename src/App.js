dd3357ac2afc2c199374598c1a875feb41697b3b
Sahajpal <sahajpal80@gmail.com>
2022-04-07 16:24:27 +0530

    implemented homeScreen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from "./HomeScreen";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+      {!user ? (
+        <Routes>
+          <Route exact path="/" element={<LoginScreen />}>          
+          </Route>
+        </Routes>
+      ): (<Routes>
+          <Route exact path="/" element={<HomeScreen />}>
+          </Route>
+        </Routes>)}
+      </Router>
     </div>
   );
 }
---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SignUpScreen.css'
+
+function SignUpScreen() {
+  return (
+    <div className='signUpScreen'></div>
+  )
+}
+
+export default SignUpScreen
