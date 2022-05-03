dc85e6cceb56a6a1dfcff941d162e92a463805ca
Sahajpal <sahajpal80@gmail.com>
2022-05-04 02:06:55 +0530

    implemented authorization and redux

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAhivccCV3aIK0SVTNjNx69yK9kJEI7AOY",
-    authDomain: "netflix-clone-76ebd.firebaseapp.com",
-    projectId: "netflix-clone-76ebd",
-    storageBucket: "netflix-clone-76ebd.appspot.com",
-    messagingSenderId: "518059927679",
-    appId: "1:518059927679:web:88786c00a71baefb33774a"
-  };
+  apiKey: "AIzaSyAhivccCV3aIK0SVTNjNx69yK9kJEI7AOY",
+  authDomain: "netflix-clone-76ebd.firebaseapp.com",
+  projectId: "netflix-clone-76ebd",
+  storageBucket: "netflix-clone-76ebd.appspot.com",
+  messagingSenderId: "518059927679",
+  appId: "1:518059927679:web:88786c00a71baefb33774a",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
