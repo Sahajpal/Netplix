d06755e98ea2e33304ac45f4e06b84c2a674c050
Sahajpal <sahajpal80@gmail.com>
2022-05-07 22:19:46 +0530

    implemented profileScreen and routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React, { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
-import LoginScreen from './screens/LoginScreen';
-import { auth } from './firebase';
-import { useDispatch, useSelector } from 'react-redux';
-import { login, logout, selectUser } from './features/counter/userSlice'
+import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/counter/userSlice";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
   const user = useSelector(selectUser);
@@ -17,31 +14,31 @@ function App() {
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
-    if (userAuth) {
-      dispatch(login({
-        uid: userAuth.uid,
-        email: userAuth.email,
-      })
-      );
-    } else {
-      dispatch(logout);
-    }
-  });
-  return unsubscribe;
-  }, []);
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return unsubscribe;
+  }, [dispatch]);
 
   return (
     <div className="app">
       <Router>
-      {!user ? (
-        <Routes>
-          <Route exact path="/" element={<LoginScreen />}>          
-          </Route>
-        </Routes>
-      ): (<Routes>
-          <Route exact path="/" element={<HomeScreen />}>
-          </Route>
-        </Routes>)}
+        {!user ? (
+          <LoginScreen/>
+        ) : (
+          <Routes>
+            <Route path="/profile" element={<ProfileScreen />}></Route>
+            <Route exact path="/" element={<HomeScreen />}></Route>
+          </Routes>
+        )}
       </Router>
     </div>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Nav.css";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Nav() {
   const [show, handleShow] = useState(false);
+  const history = useNavigate();
 
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
@@ -22,11 +24,13 @@ function Nav() {
     <div className={`nav ${show && "nav_black"}`}>
       <div className="nav_contents">
         <img
+          onClick={() => history("/")}
           className="nav_logo"
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
         <img
+          onClick={() => history("/profile")}
           className="nav_avatar"
           src="https://i.pinimg.com/originals/2b/90/0d/2b900d5612554cd0b5edf7d8e848c3ea.png"
           alt=""
